08d63ed24348a923f318eefd4b1dae0ebf7c8637
刘天和 <lth9804@bupt.edu.cn>
2024-06-13 16:26:42 +0800

    new

--- result/statistic.py
+++ result/statistic.py
@@ -25,15 +25,15 @@ class Statistic:
         self.block_rate_sec_req_calls = 0.0     # 安全需求业务阻塞率
         self.block_rate_norm_req_calls = 0.0    # 普通需求业务阻塞率
 
-        self.mean_hop = 0.0                     # 平均路径跳数
-        self.mean_hop_sec_req_calls = 0.0       # 平均安全业务跳数
-        self.mean_hop_norm_req_calls = 0.0      # 平均普通业务跳数
-        self.mean_hop_working_path = 0.0        # 平均工作路径跳数
-        self.mean_hop_backup_path = 0.0         # 平均保护路径跳数
-        self.mean_hop_working_path_sec_req = 0.0# 平均安全业务工作路径跳数
-        self.mean_hop_backup_path_sec_req = 0.0 # 平均安全业务保护路径跳数
-        self.mean_hop_working_path_norm_req = 0.0# 平均普通业务工作路径跳数
-        self.mean_hop_backup_path_norm_req = 0.0# 平均普通业务保护路径跳数
+        self.mean_hop = 0.0                         # 平均路径跳数
+        self.mean_hop_sec_req_calls = 0.0           # 平均安全业务跳数
+        self.mean_hop_norm_req_calls = 0.0          # 平均普通业务跳数
+        self.mean_hop_working_path = 0.0            # 平均工作路径跳数
+        self.mean_hop_backup_path = 0.0             # 平均保护路径跳数
+        self.mean_hop_working_path_sec_req = 0.0    # 平均安全业务工作路径跳数
+        self.mean_hop_backup_path_sec_req = 0.0     # 平均安全业务保护路径跳数
+        self.mean_hop_working_path_norm_req = 0.0   # 平均普通业务工作路径跳数
+        self.mean_hop_backup_path_norm_req = 0.0    # 平均普通业务保护路径跳数
 
         self.mean_num_high_tapping_risk = 0.0       # 平均高窃听风险数量
         self.mean_num_joint_tapping_risk = 0.0      # 平均共享窃听风险数量
@@ -48,13 +48,14 @@ class Statistic:
 
     def snapshot(self, event: Event, status: bool, G: nx.MultiDiGraph, routeTable: dict):
         self.time_stamp.append(event.time)
-        self._update_num_calls(event, status)
-        self._update_block_rate()
-
         if event.type == "callArrive":
+            self._update_num_calls(event, status)
+            self._update_block_rate()
+            self._update_hop(event, status, routeTable)
+            self._update_tapping_risk(event, status, routeTable, G)
+
             if status:
                 # 若业务成功开通
-                self.mean_hop = np.mean([self.mean_hop, len(routeTable[event.call.id]["workingPath"])]) if self.mean_hop else len(routeTable[event.call.id]["workingPath"])
                 self.realtime_num_carried_calls += 1
                 if event.call.requestSecurity:
                     self.realtime_num_carried_sec_calls += 1
@@ -86,13 +87,7 @@ class Statistic:
             else:
                 # 若阻塞业务离去
                 pass
-        self.time_stamp.append(event.time)
-        self.realTimeCallsCarried.append(self.realtime_num_carried_calls)
-        self.realTimeCallsBlocked.append(self.currentCallsBlockNum)
-        self.realTimeSecurityCallsCarried.append(self.realtime_num_carried_sec_calls)
-        self.realTimeSecurityCallsBlocked.append(self.currentSecurityCallsBlockNum)
-        self.realTimeNormalCallsCarried.append(self.realtime_num_carried_norm_calls)
-        self.realTimeNormalCallsBlocked.append(self.currentNormalCallsBlockNum)
+
         self.mean_link_utilization.append(self._calLinkUtilization(G))
 
     def show(self):
@@ -167,25 +162,65 @@ class Statistic:
             raise Exception("The sum of norm calls of the success rate {} and the blocking rate {} is not 100%."
                             .format(self.success_rate_norm_req_calls, self.block_rate_norm_req_calls))
 
-    def _update_hop(self):
-        pass
+    def _update_hop(self, event: Event, status: bool, routing_table: dict):
+        if status:
+            hop_working_path = len(routing_table[event.call.id]["workingPath"])
+            hop_backup_path = len(routing_table[event.call.id]['backupPath'])
+            mean_hop_call = self._mean(hop_working_path, hop_backup_path)
+            self.mean_hop = self._mean(self.mean_hop, mean_hop_call)
+            self.mean_hop_working_path = self._mean(self.mean_hop_working_path, hop_working_path)
+            self.mean_hop_backup_path = self._mean(self.mean_hop_backup_path, hop_backup_path)
+            if event.call.requestSecurity:
+                self.mean_hop_sec_req_calls = self._mean(self.mean_hop_sec_req_calls, mean_hop_call)
+                self.mean_hop_working_path_sec_req = self._mean(self.mean_hop_working_path_sec_req, hop_working_path)
+                self.mean_hop_backup_path_sec_req = self._mean(self.mean_hop_backup_path_sec_req, hop_backup_path)
+            else:
+                self.mean_hop_norm_req_calls = self._mean(self.mean_hop_norm_req_calls, mean_hop_call)
+                self.mean_hop_working_path_norm_req = self._mean(self.mean_hop_working_path_norm_req, hop_working_path)
+                self.mean_hop_backup_path_norm_req = self._mean(self.mean_hop_backup_path_norm_req, hop_backup_path)
+        else:
+            pass
 
-    def _calLinkUtilization(self, G: nx.MultiDiGraph):
-        linkUtilization = 0
+    def _update_tapping_risk(self, event: Event, status: bool, routing_table: dict, G: nx.MultiDiGraph):
+        """
+        self.mean_num_high_tapping_risk = 0.0       # 平均高窃听风险数量
+        self.mean_num_joint_tapping_risk = 0.0      # 平均共享窃听风险数量
+        self.mean_level_high_tapping_risk = 0.0     # 平均高窃听风险度（%）
+        self.mean_level_joint_taping_risk = 0.0     # 平均共享窃听度(%)
+        """
+        def calLinkRisky(self, G: nx.MultiDiGraph, path: list):
+            riskLinkNum = 0
+            for (start, end, index) in path:
+                for risk in G[start][end][index]["risk"]:
+                    if "H" in risk:
+                        riskLinkNum += 1
+            return riskLinkNum / len(path) * 100
+        if status:
+            if event.call.requestSecurity:
+                pass
+            else:
+                pass
+        else:
+            pass
+
+    def _update_link_utilization(self, G: nx.MultiDiGraph):
+        lu = 0.0
         for (start, end, index) in G.edges:
-            linkUtilization += 1 - G[start][end][index]["bandwidth"] / G[start][end][index]["max-bandwidth"]
-        return linkUtilization / len(G.edges)
-
-    def _calLinkRisky(self, G: nx.MultiDiGraph, path: list):
-        riskLinkNum = 0
-        for (start, end, index) in path:
-            for risk in G[start][end][index]["risk"]:
-                if "H" in risk:
-                    riskLinkNum += 1
-        return riskLinkNum / len(path) * 100
+            lu += (1 - G[start][end][index]["bandwidth"] / G[start][end][index]["max-bandwidth"]) * 100
+        lu = lu / len(G.edges)
+        self.mean_link_utilization = self._mean(self.mean_link_utilization, lu)
+        # 校验
+        if self.mean_link_utilization > 100 and self.mean_link_utilization < 0:
+            raise Exception("")
 
     def _calJointRisk(self, G: nx.MultiDiGraph, workingPath: list, backupPath: list):
         workingPathRisk = [G[s][d][i]["risk"] for (s, d, i) in workingPath]
         backupPathRisk = [G[s][d][i]["risk"] for (s, d, i) in backupPath]
         jointRiskNum = len([risk for risk in workingPathRisk if risk in backupPathRisk])
         return jointRiskNum
+
+    def _mean(self, a, b):
+        if a * b == 0:
+            return a + b
+        else:
+            return np.mean([a, b])
