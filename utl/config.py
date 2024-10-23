97a6b0ea09eb2d6518ec11992862220588418167
刘天和 <lth9804@bupt.edu.cn>
2024-10-23 11:15:45 +0800

    remake

---
+++ utl/config.py
@@ -0,0 +1,16 @@
+
+
+class Config:
+    """
+    读取配置文件，存储字条信息
+    """
+    def __init__(self):
+        pass
+
+    def read(self, config_file: str):
+        """
+        输入：配置文件路径
+        输出：返回configer
+        读取算法配置、拓扑配置、事件配置、流量配置
+        """
+        pass
