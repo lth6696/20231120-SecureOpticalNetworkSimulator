d352f8445ecff044baadf03adbe46520b862faa3
TheoLiu <lth9804@bupt.edu.cn>
2024-10-25 00:14:30 +0800

    remake

--- simulator.py
+++ simulator.py
@@ -12,46 +12,49 @@ import pandas as pd
 import event
 import network
 import result
+import utl
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
 
-def simulator(configFile: str):
+def simulator(config_file: str):
     # 检查输入
-    if not os.path.exists(configFile):
+    if not os.path.exists(config_file):
         raise Exception("Config file does not exist.")
-    # 生成流量
-    tfc_gen = network.traffic.TrafficGenerator(configFile)
-    tfc_gen.set_static_traffic()
-    # 生成物理拓扑
-    logging.info("{} - {} - Construct the physical topology.".format(__file__, __name__))
-    physicalTopology = network.topology.PhysicalTopology()
-    physicalTopology.constructGraph(configFile)
-    physicalTopology.route(tfc_gen.calls, weight="weight")
-    logging.info("{} - {} - Done.".format(__file__, __name__))
-    # 生成离散事件器
-    scheduler = event.scheduler.Scheduler()
-    # 生成攻击事件
-    logging.info("{} - {} - Generate the attack events.".format(__file__, __name__))
-    ai = network.info.AreaInfo(configFile)
-    area_info = ai.get(physicalTopology)
-    atks = network.generator.Generator()
-    atks.generate(configFile, scheduler, ai, "random")
-    logging.info("{} - {} - Done.".format(__file__, __name__))
-    # 加载数据统计模块
-    logging.info("{} - {} - Load the statistic module.".format(__file__, __name__))
-    statistic = result.statistic.Statistic()
-    # 启动管控平台
-    logging.info("{} - {} - Start the control plane.".format(__file__, __name__))
-    controller = network.controller.ControlPlane(configFile)
-    controller.run(scheduler, physicalTopology, ai, statistic)
-    logging.info("{} - {} - Done.".format(__file__, __name__))
-    # 返回仿真结果
-    # result.curve.PlotCurve.plotRealTime(statistic.time_stamp, statistic.realtime_num_carried_calls)
-    # result.curve.PlotCurve.plotRealTime(statistic.time_stamp, statistic.realtime_attacks)
-    return statistic.content_displayable_results, statistic.get()
+    configer = utl.config.Config()
+    configer.read(config_file)
+    # # 生成流量
+    # tfc_gen = network.traffic.TrafficGenerator(config_file)
+    # tfc_gen.set_static_traffic()
+    # # 生成物理拓扑
+    # logging.info("{} - {} - Construct the physical topology.".format(__file__, __name__))
+    # physicalTopology = network.topology.PhysicalTopology()
+    # physicalTopology.constructGraph(config_file)
+    # physicalTopology.route(tfc_gen.calls, weight="weight")
+    # logging.info("{} - {} - Done.".format(__file__, __name__))
+    # # 生成离散事件器
+    # scheduler = event.scheduler.Scheduler()
+    # # 生成攻击事件
+    # logging.info("{} - {} - Generate the attack events.".format(__file__, __name__))
+    # ai = network.info.AreaInfo(config_file)
+    # area_info = ai.get(physicalTopology)
+    # atks = network.generator.Generator()
+    # atks.generate(config_file, scheduler, ai, "random")
+    # logging.info("{} - {} - Done.".format(__file__, __name__))
+    # # 加载数据统计模块
+    # logging.info("{} - {} - Load the statistic module.".format(__file__, __name__))
+    # statistic = result.statistic.Statistic()
+    # # 启动管控平台
+    # logging.info("{} - {} - Start the control plane.".format(__file__, __name__))
+    # controller = network.controller.ControlPlane(config_file)
+    # controller.run(scheduler, physicalTopology, ai, statistic)
+    # logging.info("{} - {} - Done.".format(__file__, __name__))
+    # # 返回仿真结果
+    # # result.curve.PlotCurve.plotRealTime(statistic.time_stamp, statistic.realtime_num_carried_calls)
+    # # result.curve.PlotCurve.plotRealTime(statistic.time_stamp, statistic.realtime_attacks)
+    # return statistic.content_displayable_results, statistic.get()
 
 
 if __name__ == '__main__':
@@ -60,23 +63,23 @@ if __name__ == '__main__':
     # 仿真配置文件
     configFile = "./topology/NSFNet.xml"
     ResultFile = "results.xlsx"
-    isSimulate = False
+    isSimulate = True
     collector = {"title": [], "results": []}
 
     # 开始仿真
     if isSimulate:
         all_res = []
         title = None
-        for i in range(30):
+        for i in range(1):
             try:
                 title, res = simulator(configFile)
                 all_res.append(res)
                 print(res)
             except:
                 continue
-        data = np.array(all_res).mean(axis=0)
-        df = pd.Series(data, index=title)
-        print(df)
+        # data = np.array(all_res).mean(axis=0)
+        # df = pd.Series(data, index=title)
+        # print(df)
     else:
         if not os.path.exists(ResultFile):
             raise Exception("File does not exist.")
--- utl/config.py
+++ utl/config.py
@@ -1,11 +1,16 @@
+import os
+import sys
+import xml.etree.ElementTree as ET
 
 
 class Config:
+    CONFIG_NAME = "simulator"
+    SIMULATOR_VERSION = "2.0"
     """
     读取配置文件，存储字条信息
     """
     def __init__(self):
-        pass
+        self.tree: ET.ElementTree = ET.ElementTree()
 
     def read(self, config_file: str):
         """
@@ -13,4 +18,19 @@ class Config:
         输出：返回configer
         读取算法配置、拓扑配置、事件配置、流量配置
         """
+        # 检查输入
+        if not os.path.exists(config_file):
+            raise Exception("Config file does not exist.")
+        self.tree = ET.parse(config_file)
+        root = self.tree.getroot()
+        if root.tag.lower() != self.CONFIG_NAME and root.attrib['version'] != self.SIMULATOR_VERSION:
+            raise AttributeError
+        # 读取属性
+        setting_names = [child.tag for child in root]
+        if input(f"Do you want to set {setting_names}?[Y/n]\n") != "Y":
+            sys.exit()
+        for name in setting_names:
+            self._set()
+
+    def _set(self):
         pass
